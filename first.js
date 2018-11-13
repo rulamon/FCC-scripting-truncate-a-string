a8a5dce33c861973bd6792613dc139c4713b9440
rulamon <ruben.lamon@gmail.com>
2018-11-13 11:15:14 +0100

    worked without a variable

--- first.js
+++ first.js
@@ -1,9 +1,8 @@
 function truncateString(str, num) {
   // Clear out that junk in your trunk
   if (str.length > num) {
-  	let newStr = `${str.slice(0, num)}...`;
-  	return newStr;
-  } else {
+  	return `${str.slice(0, num)}...`;
+  	} else {
   	return str;
   }
 }
